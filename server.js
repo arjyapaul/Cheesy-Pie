e35fd252359365d8322916413805d474e0a829f3
Arjya Paul <arjyapaul14@gmail.com>
2022-04-04 00:39:19 +0530

    connected database via mongodb and added dynamic menu from database

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const app=express()
 const ejs=require('ejs')
 const expressLayout=require('express-ejs-layouts')
 const path=require('path')
+const mongoose=require('mongoose')
 const PORT= process.env.PORT || 5000
 
+//database connection
+const url='mongodb://localhost/pizza';
+mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology:true});
+const connection=mongoose.connection;
+connection.once('open',()=>{
+    console.log('Database connected...');
+}).on('error',function(err){
+    console.log('Database disconnected...')
+});
 //assets
 app.use(express.static('public'))
 
