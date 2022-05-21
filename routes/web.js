e653b919f91a0fa7aad1a707514a01ace114ef9d
Arjya Paul <arjyapaul14@gmail.com>
2022-05-22 00:33:00 +0530

    session and dynamic cart implemented

--- routes/web.js
+++ routes/web.js
@@ -7,5 +7,6 @@ function initRoutes(app){
     app.get('/cart',cartController().index)
     app.get('/login',authController().login)
     app.get('/register',authController().register)
+    app.post('/update-cart',cartController().update)
 }
 module.exports=initRoutes;
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 const express=require('express')
 const app=express()
 const ejs=require('ejs')
 const expressLayout=require('express-ejs-layouts')
 const path=require('path')
 const mongoose=require('mongoose')
+const session=require('express-session')
+const flash=require('express-flash')
+const MongoDbStore=require('connect-mongo')
 const PORT= process.env.PORT || 5000
 
 //database connection
@@ -15,8 +19,35 @@ connection.once('open',()=>{
 }).on('error',function(err){
     console.log('Database disconnected...')
 });
+
+//session store
+// let mongoStore=new MongoDbStore({
+//     mongooseConnection:connection,
+//     collection:'sessions'
+// })
+
+//session config
+app.use(session({
+    secret:process.env.COOKIE_SECRET,
+    resave:false,
+    store:MongoDbStore.create({
+        mongoUrl:url
+    }),
+    saveUninitialized:false,
+    cookie:{maxAge:1000*60*60*24} //24 hours
+}))
+
+app.use(flash())
+
 //assets
 app.use(express.static('public'))
+app.use(express.json())
+
+//global middleware
+app.use((req,res,next)=>{
+    res.locals.session=req.session
+    next()
+})
 
 //set template engine
 app.use(expressLayout)
