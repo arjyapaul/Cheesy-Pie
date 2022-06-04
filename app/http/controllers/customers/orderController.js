2ae5d41895b4729c697906232ba56f5b4d672c0c
Arjya Paul <arjyapaul14@gmail.com>
2022-06-04 14:02:33 +0530

    order functionality, admin - customer table created

--- app/http/controllers/customers/orderController.js
+++ app/http/controllers/customers/orderController.js
@@ -1,4 +1,5 @@
 const Order=require('../../../models/order')
+const moment=require('moment')
 function orderController(){
     return{
         store(req,res){
@@ -16,11 +17,17 @@ function orderController(){
             })
             order.save().then(result=>{
                 req.flash('success','order placed successfully')
-                return res.redirect('/')
+                delete req.session.cart
+                return res.redirect('/customer/orders')
             }).catch(err=>{
                 req.flash('error','something went wrong')
                 return res.redirect('/cart')
             })
+        },
+        async index(req,res){
+            const orders=await Order.find({customerId:req.user._id},null,{sort:{'createdAt':-1}})
+            res.header('Cache-Control','no-cache,private,no-store,nust-revalidate,max-stale=0,post-check=0,pre-check=0')
+            res.render('customers/orders',{orders:orders,moment:moment})
         }
     }
 }
--- routes/web.js
+++ routes/web.js
@@ -3,6 +3,9 @@ const cartController = require('../app/http/controllers/customers/cartController
 const homeController=require('../app/http/controllers/homeController')
 const orderController=require('../app/http/controllers/customers/orderController')
 const guest=require('../app/http/middlewares/guest')
+const auth=require('../app/http/middlewares/auth')
+const admin=require('../app/http/middlewares/admin')
+const AdminOrderController=require('../app/http/controllers/admin/orderController')
 function initRoutes(app){
     app.get('/',homeController().index)
     app.get('/cart',cartController().index)
@@ -12,6 +15,11 @@ function initRoutes(app){
     app.post('/register',authController().postRegister)
     app.post('/logout',authController().logout)
     app.post('/update-cart',cartController().update)
-    app.post('/orders',orderController().store)
+    //customer routes
+    app.post('/orders',auth,orderController().store)
+    app.get('/customer/orders',auth,orderController().index)
+
+    //admin routes
+    app.get('/admin/orders',admin,AdminOrderController().index)
 }
 module.exports=initRoutes;
